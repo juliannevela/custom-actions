1c0a30589ccc64ea5d51b6d6eb838594e3593ba9
Juli Vela <julianne.vela@brewlogix.com>
2022-08-08 12:46:27 -0700

    added tests for createApiUrl helper hook - all passing

---
+++ __tests__/createUrl.test.ts
@@ -0,0 +1,39 @@
+import { expect, it } from '@jest/globals'
+import { TP_API_ENDPOINTS } from '../src/config/tpio.config'
+import useCreateUrl from '../src/hooks/useCreateUrl'
+
+describe('Create URL Hook', () => {
+    it('should create a valid TestProject URL with the given params: Jobs Run', () => {
+        const jobRunUrl = {
+            endpoint: 'JOB_RUN',
+            projectId: 'JobRunProjectId',
+            jobId: 'JobRunJobId',
+        }
+
+        expect(useCreateUrl(jobRunUrl)).toBe(
+            `https://api.testproject.io/v2/projects/${jobRunUrl.projectId}/jobs/${jobRunUrl.jobId}/run`
+        )
+    })
+    it('should create a valid TestProject URL with the given params: Test Run', () => {
+        const testRunUrl = {
+            endpoint: 'TEST_RUN',
+            projectId: 'TestRunProjectId',
+            testId: 'TestRunTestId',
+        }
+        expect(useCreateUrl(testRunUrl)).toBe(
+            `https://api.testproject.io/v2/projects/${testRunUrl.projectId}/tests/${testRunUrl.testId}/run`
+        )
+    })
+    it('should create a valid TestProject URL with the given params: Test Execution State', () => {
+        const testExecutionStateUrl = {
+            endpoint: 'TEST_EXECUTION_STATE',
+            projectId: 'TestExecutionStateProjectId',
+            testId: 'TestExecutionStateTestId',
+            executionId: 'TestExecutionStateExecutionId',
+        }
+
+        expect(useCreateUrl(testExecutionStateUrl)).toBe(
+            `https://api.testproject.io/v2/projects/${testExecutionStateUrl.projectId}/tests/${testExecutionStateUrl.testId}/executions/${testExecutionStateUrl.executionId}/state`
+        )
+    })
+})
--- src/hooks/useCreateUrl.ts
+++ src/hooks/useCreateUrl.ts
@@ -10,45 +10,62 @@ interface IApiProps {
 
 const useCreateUrl = (APIProps: IApiProps) => {
     let url = ''
+    let endpoint = ''
+
     if (!APIProps.projectId) throw new Error('Project ID is required')
+
     switch (APIProps.endpoint) {
-        case TP_API_ENDPOINTS.PROJECTS:
-            const parsedEndpoint = APIProps.endpoint.replace('$1', APIProps.projectId)
+        case 'PROJECTS':
+            endpoint = TP_API_ENDPOINTS.PROJECTS
+            const parsedEndpoint = endpoint.replace('$1', APIProps.projectId)
             url = `${BASE_URL}${parsedEndpoint}`
-        case TP_API_ENDPOINTS.TESTS:
-            const parsedTestEndpoint = APIProps.endpoint.replace('$1', APIProps.projectId)
+            break
+        case 'TESTS':
+            endpoint = TP_API_ENDPOINTS.TESTS
+            const parsedTestEndpoint = endpoint.replace('$1', APIProps.projectId)
             url = `${BASE_URL}${parsedTestEndpoint}`
-        case TP_API_ENDPOINTS.TEST_RUN:
+            break
+        case 'TEST_RUN':
+            endpoint = TP_API_ENDPOINTS.TEST_RUN
             if (!APIProps.testId) throw new Error('Test ID is required')
-            const parsedTestRunEndpoint = APIProps.endpoint
+            const parsedTestRunEndpoint = endpoint
                 .replace('$1', APIProps.projectId)
                 .replace('$2', APIProps.testId)
             url = `${BASE_URL}${parsedTestRunEndpoint}`
-        case TP_API_ENDPOINTS.TEST_EXECUTION_STATE:
+            break
+        case 'TEST_EXECUTION_STATE':
+            endpoint = TP_API_ENDPOINTS.TEST_EXECUTION_STATE
             if (!APIProps.executionId || !APIProps.testId)
                 throw new Error(`${!APIProps.testId ? 'Test ID' : 'Execution ID'} is required`)
-            const parsedTestExecutionStateEndpoint = APIProps.endpoint
+            const parsedTestExecutionStateEndpoint = endpoint
                 .replace('$1', APIProps.projectId)
                 .replace('$2', APIProps.testId)
                 .replace('$3', APIProps.executionId)
             url = `${BASE_URL}${parsedTestExecutionStateEndpoint}`
-        case TP_API_ENDPOINTS.JOBS:
-            const parsedJobsEndpoint = APIProps.endpoint.replace('$1', APIProps.projectId)
+            break
+        case 'JOBS':
+            endpoint = TP_API_ENDPOINTS.JOBS
+            const parsedJobsEndpoint = endpoint.replace('$1', APIProps.projectId)
             url = `${BASE_URL}${parsedJobsEndpoint}`
-        case TP_API_ENDPOINTS.JOB_RUN:
+            break
+        case 'JOB_RUN':
+            endpoint = TP_API_ENDPOINTS.JOB_RUN
             if (!APIProps.jobId) throw new Error('Job ID is required')
-            const parsedJobRunEndpoint = APIProps.endpoint
+            const parsedJobRunEndpoint = endpoint
                 .replace('$1', APIProps.projectId)
                 .replace('$2', APIProps.jobId)
             url = `${BASE_URL}${parsedJobRunEndpoint}`
-        case TP_API_ENDPOINTS.JOB_EXECUTION_STATE:
+            break
+        case 'JOB_EXECUTION_STATE':
+            endpoint = TP_API_ENDPOINTS.JOB_EXECUTION_STATE
             if (!APIProps.executionId || !APIProps.jobId)
                 throw new Error(`${!APIProps.jobId ? 'Job ID' : 'Execution ID'} is required`)
-            const parsedJobExecutionStateEndpoint = APIProps.endpoint
+            const parsedJobExecutionStateEndpoint = endpoint
                 .replace('$1', APIProps.projectId)
                 .replace('$2', APIProps.jobId)
                 .replace('$3', APIProps.executionId)
             url = `${BASE_URL}${parsedJobExecutionStateEndpoint}`
+            break
         default:
             throw new Error('Invalid endpoint')
     }
